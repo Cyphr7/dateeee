f3eb3b06a044936859452dc0b4c262af1ce38f57
Cyphr7 <148668424+Cyphr7@users.noreply.github.com>
2023-11-28 19:51:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 9th!";
+  question.innerHTML = "Yay, see you on the 8th!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
